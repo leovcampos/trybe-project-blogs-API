9f1495585bcdf98eee8b2fcba704581de7e87aa6
leovcampos <leonardoviniciuscampos@gmail.com>
2022-10-27 17:29:52 -0300

    req10 progress

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -1,44 +1,44 @@
 const BlogPostModel = (sequelize, DataTypes) => {
     const BlogPost = sequelize.define('BlogPost', {
         id: {
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
         },
         title: DataTypes.STRING,
         content: DataTypes.STRING,
         userId: {
-          type: DataTypes.INTEGER,
-          foreignKey: true,
+            type: DataTypes.INTEGER,
+            foreignKey: true,
         },
         updated: {
-          type: DataTypes.DATE,
-          allowNull: false,
-          defaultValue: new Date(),
+            type: DataTypes.DATE,
+            defaultValue: new Date(),
+            allowNull: false,
         },
         published: {
-          type: DataTypes.DATE,
-          allowNull: false,
-          defaultValue: new Date(),
+            type: DataTypes.DATE,
+            defaultValue: new Date(),
+            allowNull: false,
         },
-      },
-      { 
-        timestamps: false,
-        underscored: true,
-        tableName: 'blog_posts',
-      },
-    );
-  
-    BlogPost.associate = (models) => {
-      BlogPost.belongsTo(
-        models.User,
+    },
         {
-          foreignKey: 'userId',
-          as: 'users',
-        }
-      );
+            timestamps: false,
+            underscored: true,
+            tableName: 'blog_posts',
+        },
+    );
+
+    BlogPost.associate = (models) => {
+        BlogPost.belongsTo(
+            models.User,
+            {
+                foreignKey: 'userId',
+                as: 'users'
+            }
+        );
     }
     return BlogPost;
-  };
-  
-  module.exports = BlogPostModel;
+};
+
+module.exports = BlogPostModel;
