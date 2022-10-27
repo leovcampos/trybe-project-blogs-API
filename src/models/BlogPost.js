1534325161cc38f3847982dc6677171d69dcde8a
leovcampos <leonardoviniciuscampos@gmail.com>
2022-10-27 14:32:31 -0300

    req10

---
+++ src/models/BlogPost.js
@@ -0,0 +1,45 @@
+const blogPostModel = (sequelize, DataTypes) => {
+    const BlogPost = sequelize.define('BlogPost', {
+        id: {
+            ype: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: DataTypes.STRING,
+        content: DataTypes.STRING,
+        userId: {
+            foreignKey: true,
+            type: DataTypes.INTEGER,
+        },
+        update: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: new Date(),
+        },
+        published: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: new Date(),
+        },
+    },
+        {
+            timestamps: false,
+            underscored: true,
+            tableName: 'blog_posts',
+        }
+    );
+
+    BlogPost.associate = (models) => {
+        BlogPost.belongsTo(
+            models.User,
+            {
+                foreignKey: userId,
+                as: users,
+            }
+        )
+    }
+
+    return BlogPost;
+};
+
+module.exports = blogPostModel;
