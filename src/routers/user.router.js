843404cffa5bd0a3d71ad621ec74548702cd2149
leovcampos <leonardoviniciuscampos@gmail.com>
2022-10-27 11:55:51 -0300

    req06 progress

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const validateUser = require('../middlewares/validate.user');
-// const validateToken = require('../middlewares/validate.token');
+const validateToken = require('../middlewares/validate.token');
 const userController = require('../controllers/user.controller');
 
 const router = express.Router();
 
 router.post('/', validateUser, userController.newUser);
-router.get('/:id', userController.getById);
+router.get('/:id', validateToken, userController.getById);
 
 module.exports = router;
